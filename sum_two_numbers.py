b4d371f1ee5ffe8be25ec14c07a83d26dceff55e
Keith Graham <grahamkeith288@gmail.com>
2025-12-18 15:04:46 +0300

    Add average calculation for three numbers

--- sum_two_numbers.py
+++ sum_two_numbers.py
@@ -1,6 +1,7 @@
 value_1 = input("First: ")
 value_2 = input("Second: ")
-
-total = float(value_1) + float(value_2)
-
+value_3 = input("Third: ")
+total = float(value_1) + float(value_2) + float(value_3)
 print("Sum:", total)
+average = total / 3
+print("Average:", average)
